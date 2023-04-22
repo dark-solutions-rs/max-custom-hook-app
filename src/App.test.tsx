0867e7779e192637752c675d651684d3988c7fb5
Darko Stevanovic <maketest@gmail.com>
2023-04-22 22:52:23 +0200

    Fixed minor typo

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders app', () => {
+test('render app', () => {
   render(<App />);
   const element = screen.getByText(/Custom Hook - useFormattedData/i);
   expect(element).toBeInTheDocument();
